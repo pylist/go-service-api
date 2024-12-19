6fdb7505d06cc1998377a159de4e64e6e499518e
pylist <bitcore@qq.com>
2024-12-19 20:39:50 +0800

    优化启动逻辑

--- main.go
+++ main.go
@@ -7,10 +7,6 @@ import (
 	"go-service-api/initialize"
 	"go-service-api/middleware"
 	"go-service-api/router"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -25,27 +21,5 @@ func main() {
 	r.Use(middleware.Cors())
 	router.Router(r)
 	addr := fmt.Sprintf("%s:%d", config.Server.Host, config.Server.Port)
-	server := &http.Server{
-		Addr:    addr,
-		Handler: r,
-	}
-
-	// 等待中断信号以优雅地关闭服务器
-	quit := make(chan os.Signal)
-	signal.Notify(quit, os.Interrupt)
-	go func() {
-		<-quit
-		go func() {
-			global.Logger.Sync()
-		}()
-		if err := server.Close(); err != nil {
-			log.Fatal("服务关闭出现错误:", err)
-		}
-	}()
-	log.Println("正在启动服务器:", addr)
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Println("服务启动错误:", err)
-	}
-
-	log.Println("服务器已退出")
+	r.Run(addr)
 }
